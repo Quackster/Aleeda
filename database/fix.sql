ff75971d4d33fc2b8f3088da6290b1c4ed8e7522
Alex Miller <TheAmazingAussie@users.noreply.github.com>
2017-09-26 18:56:14 +1000

    Add database

---
+++ database/fix.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE IF NOT EXISTS `private_rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `rating` int(5) NOT NULL,
+  `description` varchar(150) NOT NULL,
+  `ownerid` int(150) NOT NULL,
+  `status` int(11) NOT NULL,
+  `tags` longtext NOT NULL,
+  `thumbnail` varchar(150) NOT NULL,
+  `petsAllowed` int(11) NOT NULL,
+  `category` int(11) NOT NULL,
+  `model` varchar(150) NOT NULL,
+  `wallpaper` int(11) NOT NULL,
+  `floorpaper` int(11) NOT NULL,
+  `landscape` varchar(111) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
